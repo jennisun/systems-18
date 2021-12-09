cc62731d83b37471c362be558cf36e5ac8da5c73
jennisun <jsun20@stuy.edu>
2021-12-08 21:46:16 -0500

    finished, fixed makefile

--- main.c
+++ main.c
@@ -8,32 +8,39 @@
 #define WRITE 1
 
 int main() {
-
+  int child[2];
   int parent[2];
+  pipe(child);
   pipe(parent);
-  printf("pipe created. parent[0]: %d parent[1]: %d\n", parent[0], parent[1]);
 
   int f;
+  char line[1000];
   f = fork();
 
   if (f) {
     //parent
     close(parent[WRITE]);
-    char line[10];
-    while (read(parent[READ], line, sizeof(line))) {
+
+    while(fgets(line, 1000, stdin)) {
+      line[strlen(line) - 1] = '\0';
+      write(child[WRITE], line, sizeof(stdin));
+      sleep(3);
+      read(parent[READ], line, sizeof(stdin));
       printf("parent got: [%s]\n", line);
     }
   }
   else {
     //child
     close(parent[READ]);
-    sleep(2);
 
-    char line[1000];
-    while(fgets(line, 1000, stdin)) {
-      line[strlen(line) - 1] = '\0';
-      write(parent[WRITE], line, sizeof(stdin));
-      sleep(3);
+    while (read(child[READ], line, sizeof(line))) {
+      int len = strlen(line);
+      char line1[len];
+      for (int i = 0; i < len; i ++) {
+        line1[len - 1 - i] = line[i];
+      }
+      line1[len] = '\0';
+      write(parent[WRITE], line1, sizeof(line1));
     }
   }
   return 0;
